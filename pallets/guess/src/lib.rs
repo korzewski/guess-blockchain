848d263a4b8ce195c0dc764badb9d7a833baee51
korzewski <korzewski@gmail.com>
2021-02-26 23:41:32 +0100

    add join_to_session()

--- pallets/guess/src/lib.rs
+++ pallets/guess/src/lib.rs
@@ -1,7 +1,15 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, traits::Get};
+use frame_support::{
+	decl_module,
+	decl_storage,
+	decl_event,
+	decl_error,
+	dispatch::{DispatchError},
+	codec::{Encode, Decode},
+};
 use frame_system::{ensure_signed};
+use sp_std::{result::{Result}};
 
 #[cfg(test)]
 mod mock;
@@ -9,91 +17,88 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
-/// Configure the pallet by specifying the parameters and types on which it depends.
 pub trait Config: frame_system::Config {
-	/// Because this pallet emits events, it depends on the runtime's definition of an event.
 	type Event: From<Event<Self>> + Into<<Self as frame_system::Config>::Event>;
 }
 
-// The pallet's runtime storage items.
-// https://substrate.dev/docs/en/knowledgebase/runtime/storage
+type SessionId = u128;
+type Bet = u32;
+type GuessValue = u8;
+
+#[derive(Encode, Decode, Default, Clone)]
+struct Session {
+	random_value: u8,
+	first_block: u32,
+	last_block: u32,
+}
+
 decl_storage! {
-	// A unique name is used to ensure that the pallet's storage items are isolated.
-	// This name may be updated, but each pallet in the runtime must use a unique name.
-	// ---------------------------------vvvvvvvvvvvvvv
 	trait Store for Module<T: Config> as Guess {
-		// Learn more about declaring storage items:
-		// https://substrate.dev/docs/en/knowledgebase/runtime/storage#declaring-storage-items
-		Something get(fn something): Option<u32>;
+		Sessions: map hasher(blake2_128_concat) SessionId => Option<Session>;
+		Bets: double_map hasher(blake2_128_concat) SessionId, hasher(blake2_128_concat) T::AccountId => Option<(GuessValue, Bet)>;
+
+		NextSessionId: SessionId;
+		CurrentSessionId get(fn session_id): u128;
 	}
 }
 
-// Pallets use events to inform users when important changes are made.
-// https://substrate.dev/docs/en/knowledgebase/runtime/events
 decl_event!(
-	pub enum Event<T> where AccountId = <T as frame_system::Config>::AccountId {
-		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
+	pub enum Event<T> where <T as frame_system::Config>::AccountId {
 		SomethingStored(u32, AccountId),
 	}
 );
 
-// Errors inform users that something went wrong.
 decl_error! {
 	pub enum Error for Module<T: Config> {
-		/// Error names should be descriptive.
-		NoneValue,
-		/// Errors should have helpful documentation associated with them.
-		StorageOverflow,
+		SessionIdOverflow,
 	}
 }
 
-// Dispatchable functions allows users to interact with the pallet and invoke state changes.
-// These functions materialize as "extrinsics", which are often compared to transactions.
-// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 decl_module! {
 	pub struct Module<T: Config> for enum Call where origin: T::Origin {
-		// Errors must be initialized if they are used by the pallet.
 		type Error = Error<T>;
 
-		// Events must be initialized if they are used by the pallet.
 		fn deposit_event() = default;
 
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[weight = 10_000 + T::DbWeight::get().writes(1)]
-		pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
-			let who = ensure_signed(origin)?;
+		#[weight = 1000]
+		pub fn join_to_session(origin, guess: GuessValue, bet: Bet) {
+			let account_id = ensure_signed(origin)?;
 
-			// Update storage.
-			Something::put(something);
-
-			// Emit an event.
-			Self::deposit_event(RawEvent::SomethingStored(something, who));
-			// Return a successful DispatchResult
-			Ok(())
+			let (session_id, _session) = Self::get_current_session()?;
+			Bets::<T>::insert(session_id, account_id, (guess, bet));
 		}
+	}
+}
 
-		/// An example dispatchable that may throw a custom error.
-		#[weight = 10_000 + T::DbWeight::get().reads_writes(1,1)]
-		pub fn cause_error(origin) -> dispatch::DispatchResult {
-			let _who = ensure_signed(origin)?;
+impl<T: Config> Module<T> {
+	fn create_new_session() -> Result<Session, DispatchError> {
+		let session_id = Self::next_session_id()?;
+		let session = Session {
+			// TODO - GENERATE RANDOM 1 -> 100
+			random_value: 8,
+			// TODO - GET CURRENT BLOCK ID
+			first_block: 0,
+			last_block: 0,
+		};
 
-			// Read a value from storage.
-			match Something::get() {
-				// Return an error if the value has not been set.
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					Something::put(new);
-					Ok(())
-				},
-			}
-		}
+		Sessions::insert(session_id, session.clone());
+		Ok(session)
+	}
+
+	fn get_current_session() -> Result<(SessionId, Session), DispatchError> {
+		let id = CurrentSessionId::get();
+
+		let session = Sessions::get(id)
+			.unwrap_or(Self::create_new_session()?);
+
+		Ok((id, session))
+	}
+
+	fn next_session_id() -> Result<SessionId, DispatchError> {
+		let session_id = NextSessionId::get();
+		let next_session_id = session_id.checked_add(1).ok_or(Error::<T>::SessionIdOverflow)?;
+		NextSessionId::put(next_session_id);
+
+		Ok(session_id)
 	}
 }
--- pallets/guess/src/tests.rs
+++ pallets/guess/src/tests.rs
@@ -1,24 +1,35 @@
 use crate::{Error, mock::*};
 use frame_support::{assert_ok, assert_noop};
 
-#[test]
-fn it_works_for_default_value() {
-	new_test_ext().execute_with(|| {
-		// Dispatch a signed extrinsic.
-		assert_ok!(Guess::do_something(Origin::signed(1), 42));
-		// Read pallet storage and assert an expected result.
-		assert_eq!(Guess::something(), Some(42));
-	});
-}
+// #[test]
+// fn it_works_for_default_value() {
+// 	new_test_ext().execute_with(|| {
+// 		// Dispatch a signed extrinsic.
+// 		assert_ok!(Guess::do_something(Origin::signed(1), 42));
+// 		// Read pallet storage and assert an expected result.
+// 		assert_eq!(Guess::something(), Some(42));
+// 	});
+// }
 
 
+// #[test]
+// fn correct_error_for_none_value() {
+// 	new_test_ext().execute_with(|| {
+// 		// Ensure the expected error is thrown when no value is present.
+// 		assert_noop!(
+// 			Guess::cause_error(Origin::signed(1)),
+// 			Error::<Test>::NoneValue,
+// 		);
+// 	});
+// }
+
 #[test]
-fn correct_error_for_none_value() {
+fn next_session_id_works() {
 	new_test_ext().execute_with(|| {
-		// Ensure the expected error is thrown when no value is present.
-		assert_noop!(
-			Guess::cause_error(Origin::signed(1)),
-			Error::<Test>::NoneValue,
-		);
+		let next_session_id = Guess::next_session_id().unwrap();
+		assert_eq!(next_session_id, 0);
+
+		let next_session_id = Guess::next_session_id().unwrap();
+		assert_eq!(next_session_id, 1);
 	});
 }
